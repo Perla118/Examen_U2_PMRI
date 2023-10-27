585cb6f41ec9d3a3d667990328fe9040ff1d93af
danye16 <danyel.parracarrillo@gmail.com>
2023-10-27 10:49:58 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Examen_PMRI.clases;
+
+namespace Examen_PMRI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            arreglos arreglos = new arreglos();
+
+            arreglos.ejercicio3();
+
+        }
+    }
+}
---
+++ clases/arreglos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_PMRI.clases
+{
+    internal class arreglos
+    {      
+        public void ejercicio3()
+        {
+            int[] desord = new int[10] {80,79,78,77,76,75,74,73,72,71};
+
+            for (int i=0; i<10; i++)
+            {
+                //Console.WriteLine(desord[i]);
+
+                if (desord[i] == 75) 
+                {
+                    Console.WriteLine($"La posicion encontrada fue: {desord[i]}");
+                }
+                else
+                    Console.WriteLine("Valor no encontrado");
+            }
+        }
+        public void tabla()
+        {
+            int[] tabla = new int[10];  
+
+        }
+    }
+}
